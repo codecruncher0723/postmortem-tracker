2c0168b4a20c70a3c22f8313a4a2aa1d636f0698
Karthik Kotian <karthikkotian0723@gmail.com>
2026-07-02 20:18:59 +0530

    feat: add vite config and redirect build output to docs folder

--- Project/src/main.ts
+++ Project/src/main.ts
@@ -1,60 +1,149 @@
-import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.ts'
+    import type { Incident, Severity } from './models/incident';
+    import { GenericStorage } from './services/storageService';
+    import { createIncident } from './utils/incidentFactory';
+    import { renderIncidentCard } from './components/IncidentCard';
+    import { countCriticalIncidents } from './utils/analytics';
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+    //1.Initialize them service
+    const incidentStorage = new GenericStorage<Incident>(
+        'postmorterm_incidents',
+        (raw: any): Incident => ({
+            ...raw,
+            createdAt: new Date(raw.createdAt),
+            resolvedAt: (raw.resolvedAt)?new Date(raw.resolvedAt): undefined
+        })
+    );
 
-<div class="ticks"></div>
+    // 2. Query the DOM safely
+    const dashboardContainer = document.getElementById('dashboard-container') as HTMLDivElement | null;
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+    if(!dashboardContainer){
+        throw new Error('Critical: Dashboard container is missing from HTML');
+    }
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+    // 3.Add incident Button
+    const addIncidentBtn = document.getElementById('add-incident-btn') as HTMLButtonElement | null;
+
+    if(!addIncidentBtn){
+        throw new Error('Critical: Add incident button is missing from HTML');
+    }
+
+    // Query DOM for critical count in dashboard container
+    const criticalCountElement = document.getElementById('critical-count') as HTMLSpanElement | null ;
+
+    if(!criticalCountElement) throw new Error('CRITICAL: Critical count span element is missing in HTML');
+
+    // Query DOM for export button
+    const exportBtn = document.getElementById('export-btn') as HTMLButtonElement | null;
+    if(!exportBtn) throw new Error('CRITICAL: Export Button is missing.');
+
+
+    // Query the file input
+    const importFileInput = document.getElementById('import-file') as HTMLInputElement | null;
+    if (!importFileInput) throw new Error('CRITICAL: Import file input missing');
+
+    // Query the search input
+    const searchInput = document.getElementById('search-input') as HTMLInputElement ;
+    if (!searchInput) throw new Error('CRITICAL:Search input is missing');
+
+    // Query the Form
+    const incidentForm = document.getElementById('incident-form') as HTMLFormElement | null;
+    if(!incidentForm){
+        throw new Error('CRITICAL: Incident Form is missing from HTML');
+    }
+
+    // Listen for the submit event
+    incidentForm.addEventListener('submit',(event:SubmitEvent) => {
+        event.preventDefault();
+
+        const target = event.target as HTMLFormElement;
+        const formData = new FormData(target);
+
+        const title = formData.get('title') as string;
+        console.log('New Incident Title: ',title);
+
+        const description = formData.get('description') as string;
+        const severity = formData.get('severity') as Severity;
+
+        const newIncident: Incident = createIncident(title, description, severity);
+
+        incidentStorage.saveNewItem(newIncident);
+
+        target.reset();
+        refreshDashboard();
+
+    })
+
+    // Dashboard Refresh function
+
+    function refreshDashboard(){
+        const allIncidents = incidentStorage.getAll();
+        const searchTerm = searchInput!.value.toLowerCase();
+        const filteredIncidents = allIncidents.filter(incident => 
+            incident.title.toLowerCase().includes(searchTerm)
+        )
+
+        const sortedIncidents = [...filteredIncidents].sort((a,b) => b.createdAt.getTime() -a.createdAt.getTime()) 
+
+        const htmlArray = sortedIncidents.map(item => renderIncidentCard(item));
+        const finalHtmlString = htmlArray.join(``);
+        dashboardContainer!.innerHTML = finalHtmlString;
+        const criticalCount = countCriticalIncidents(filteredIncidents);
+        criticalCountElement!.innerText = criticalCount.toString();
+
+
+    }
+    refreshDashboard();
+
+    //Export Button Event Listener
+    exportBtn.addEventListener('click', () => {
+        incidentStorage.exportToJSON();
+    })
+
+    //Listen for when the user selects the file
+    importFileInput.addEventListener('change', (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        const file = target.files?.[0];
+        if(!file) return;
+        const reader = new FileReader();
+
+        reader.onload = (e: ProgressEvent<FileReader>) => {
+            const jsonString = e.target?.result as string;
+            // We have the text! Now we need a method to handle it.
+            incidentStorage.importJSON(jsonString);
+            refreshDashboard();
+        }
+        reader.readAsText(file);
+
+    });
+
+    // Listen for every key stroke
+    searchInput.addEventListener('input', () => {
+        refreshDashboard();
+    })
+
+    //Listen for delete button click from dashboard container
+    dashboardContainer.addEventListener('click', (event: MouseEvent) => {
+        const target = event.target as HTMLElement;
+        if(target.matches('.delete-btn')){
+            const card = target.closest('.incident-card') as HTMLElement | null;
+            if(card && card.dataset.id){
+                const incidentId = card.dataset.id;
+                const isConfirmed = window.confirm("Are you sure you want to delete this incident?");
+                if(isConfirmed){
+                    incidentStorage.deleteItem(incidentId);
+                    refreshDashboard();
+                }
+            }
+        }
+        else if(target.matches('.resolve-btn')){
+            const card = target.closest('.incident-card') as HTMLElement | null;
+            if(card && card.dataset.id){
+                const incidentId = card.dataset.id;
+                incidentStorage.updateItem(incidentId,{ status: 'RESOLVED', resolvedAt: new Date() });
+                refreshDashboard();
+            }
+        }
+    });
---
+++ Project/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // This helps Vite handle the subfolder structure correctly
+  base: './', 
+  build: {
+    // This tells Vite to output the build files to a 'docs' folder 
+    // located at the root of your repository (PostMortem/docs)
+    outDir: '../docs', 
+    emptyOutDir: true,
+  },
+});
